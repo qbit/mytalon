b00f52b379875ab5053cf6291b0e877a9ecb6acb
Aaron Bieber <aaron@bolddaemon.com>
2022-12-23 07:24:45 -0700

    Disable mic on startup, message when done.

--- settings.py
+++ settings.py
@@ -1,6 +1,11 @@
 from talon import app, actions
 
-def disable_speech_on_start():
+def disable_on_start():
     actions.speech.disable()
+    actions.speech.set_microphone('None')
 
-app.register("ready", disable_speech_on_start)
+app.register("ready", disable_on_start)
+
+app.notify(title="Talon startup complete!",
+           subtitle="Speech and MIC are disabled.\nHappy hacking!",
+           sound=True)
